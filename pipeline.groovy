c80e44ef5dede1d7b317b8492c2fa495ed5fc30e
SakshiSengar <119332066+SakshiSengar@users.noreply.github.com>
2023-03-15 13:33:11 +0530

    Added pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,11 @@
+def divide(int x, int y) {
+    if (y == 0) {
+        return "Should not divide by 0"
+    } else {
+        return x/y
+    }
+}
+def greetMsg() {
+    println "Hello! I am Groovy"
+}
+return this
