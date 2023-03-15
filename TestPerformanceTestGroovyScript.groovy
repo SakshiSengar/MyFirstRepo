0cb24bb858922e4c1d71f2b0cd277d2b256722a6
SakshiSengar <119332066+SakshiSengar@users.noreply.github.com>
2023-03-15 14:40:21 +0530

    added regressionTestResult files from the article

---
+++ TestPerformanceTestGroovyScript.groovy
@@ -0,0 +1,69 @@
+import groovy.json.JsonSlurper
+import groovy.json.JsonSlurper
+def inputFile = new File("C:\\IX\\jenkins\\service\\results\\regressiontestresults.json")
+def InputJSON = new JsonSlurper().parseText(inputFile.text)
+def writer = new StringWriter() // html is written here by markup builder
+def markup = new groovy.xml.MarkupBuilder(writer) // the builder
+String id1 = InputJSON.get("Results")
+def id2 = InputJSON.get("Results")
+markup.html {
+  head {
+   style(type: "text/css", ''
+    '
+    .failedtest {
+     color: red;
+     text - align: center;
+    }
+    ''
+    ')
+   }
+   markup.body {
+    p "Hi All,"
+    p "Please analyze the failures and add your analysis in the remarks column."
+    p "Note: This is the first version of the automatic email, we shall keep enhancing the format for automatic color coding and grouping of data."
+   } {
+    markup.table(border: "1") {
+     markup.thead(bgcolor: "blue") {
+      markup.tr {
+       markup.th("Team")
+       markup.th("Profiles")
+       markup.th("FrameWork")
+       markup.th("Total Tests")
+       markup.th("Passed")
+       markup.th("Failed")
+       markup.th("Skipped")
+       markup.th("Results URL")
+       markup.th("Date")
+       markup.th("Remarks")
+      } // tr
+     } // thead
+     def dateinhtml
+     def teamName
+     markup.tbody {
+      markup.tr {
+       for (def data: id2) {
+        markup.tr {
+         teamName = data.pyTextValue.toString()
+         markup.td(align: "left", teamName.substring(1, teamName.length() - 1))
+         markup.td(align: "left", data.Profile)
+         markup.td(align: "left", data.ToolRunner)
+         markup.td(align: "center", data.TotalTests)
+         markup.td(align: "center", data.Passed)
+         if (data.Failed.toInteger() == 0) {
+          markup.td(align: "center", data.Failed)
+         } else {
+          markup.td('class': 'failedtest', data.Failed)
+         }
+         markup.td(align: "center", data.Skipped)
+         markup.td(align: "left", data.ResultsURL)
+         //  markup.td(align:"right",data.DateTime)
+         dateinhtml = data.DateTimeValue.toString()
+         markup.td(align: "right", dateinhtml.substring(1, 11))
+        } // foreach
+       } // td
+      } // tr
+     } //tbody
+    } // table
+   }
+   def newFile = new File("C://IX//jenkins//service//results//regressiontestresults.html")
+   newFile.write(writer.toString())
